1f6392312ce0a9b2877ed8fb36cde9823c62422c
Peter Jausovec <peter.jausovec@solo.io>
2025-02-17 16:53:32 -0800

    more istio tools
    Signed-off-by: Peter Jausovec <peter.jausovec@solo.io>

--- python/src/kagent/tools/istio/__init__.py
+++ python/src/kagent/tools/istio/__init__.py
@@ -1,7 +1,33 @@
-from ._istio_crds import GenerateResource
+from ._istio_crds import Config, IstioCRDTool
 from ._istioctl import (
+    AnalyzeClusterConfig,
+    ApplyWaypoint,
+    DeleteWaypoint,
+    GenerateManifest,
+    GenerateWaypoint,
+    InstallIstio,
+    ListWaypoints,
     ProxyConfig,
-    VerifyInstall,
+    ProxyStatus,
+    RemoteClusters,
+    WaypointStatus,
+    ZTunnelConfig,
 )
 
-__all__ = ["ProxyConfig", "VerifyInstall", "GenerateResource"]
+__all__ = [
+    "AnalyzeClusterConfig",
+    "ApplyWaypoint",
+    "DeleteWaypoint",
+    "GenerateManifest",
+    "GenerateResource",
+    "GenerateWaypoint",
+    "InstallIstio",
+    "ListWaypoints",
+    "ProxyConfig",
+    "ProxyStatus",
+    "RemoteClusters",
+    "WaypointStatus",
+    "ZTunnelConfig",
+    "IstioCRDTool",
+    "Config",
+]
